a5f4564d7752338dea650f3d2afd9045761e49a4
Tónga <gastongracis@gmail.com>
2023-08-16 20:36:02 -0300

    borro la config que no hace falta

--- db/back4app-config.js
+++
@@ -1,7 +0,0 @@
-import Parse from "parse";
-
-const PARSE_APPLICATION_ID = "WNBfJEeklSm2WQ7p92cJDtiPs7lpJyrkUErWj2uJ";
-const PARSE_HOST_URL = "https://parseapi.back4app.com/";
-const PARSE_JAVASCRIPT_KEY = "bMSVQiyAwrqenmqdhc11M7gl1BcvlXEX5H1nJcoX";
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
