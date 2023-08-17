ac46186165f7cd5ff504c9b05e6d8b00d597aa89
Matias Garcia <64979089+OneWhisper31@users.noreply.github.com>
2023-08-17 16:03:48 -0300

    Screen Css 1.0
    Estaría bueno que el usuario pueda tener un fondo para subir y que nosotros con una función decidamos si usamos un tema oscuro o un tema claro.

--- src/components/screens/Screen1/index.jsx
+++ src/components/screens/Screen1/index.jsx
@@ -1,7 +1,16 @@
-import { Box, Text, Stack } from "@chakra-ui/react";
+import { Box, Text, SimpleGrid, useColorMode, Badge } from "@chakra-ui/react";
+
 import React, { useEffect } from "react";
 
 const ListadoProductos = ({ productos, onFetchProductos }) => {
+
+  const fontText = ["arial", "Rowdies", "Caprasimo", "Courier Prime"];
+
+  const productosArray = () => { let _ar = new Array(0); productos.map((x) => (_ar.push(x))); return _ar; }
+  const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
   useEffect(() => {
     fetchProductos();
   }, []);
@@ -11,27 +20,55 @@ const ListadoProductos = ({ productos, onFetchProductos }) => {
   };
 
   return (
-    <Box maxW="700px" mx="auto" mt="4">
-      <Text color="gray.500" fontSize="2em" mb="4">
-        Listado de productos
+    <Box>
+      <Text
+        color={(colorMode === 'light' ? 'black' : 'White')}
+        fontSize="2em"
+        mb="4"
+        textAlign="center"
+        textTransform="uppercase"
+        fontFamily={["arial", "Rowdies", "Caprasimo"]}
+        mt="1em"
+      >
+        PANADERIA NTQJ
       </Text>
-      <Stack spacing="4">
+      <SimpleGrid
+        columns={clamp(Math.ceil(productosArray().length / 3), 1, 4)}
+        spacing="1em"
+        m="2em 5em"
+      >
         {productos.map((producto) => (
           <Box
             key={producto.objectId}
             p="4"
             borderWidth="1px"
+            border
             borderRadius="md"
             boxShadow="sm"
+            paddingLeft='3em'
+            bg={(colorMode === 'light' ? '#353535' : '#e6e6e6')}
+            color={(colorMode === 'light' ? 'white' : 'black')}
           >
-            <Text fontWeight="bold" fontSize="lg">
-              {producto.item} - Precio: ${producto.precio}
+            <Text fontWeight="bold" fontSize="x-large" fontFamily={fontText}>
+              {producto.item}
             </Text>
-            <Text>Categoría: {producto.categoria}</Text>
-            <Text>Descripción: {producto.descripcion}</Text>
+            <Badge
+              variant='solid'
+              colorScheme='green'
+              fontFamily={["arial", "Rowdies", "Caprasimo"]}
+              w='9em'
+              textAlign='center'
+              fontWeight='extrabold'
+              color={(colorMode === 'light' ? 'white' : 'black')}
+            >
+              {producto.categoria}</Badge>
+            <Text fontFamily={fontText} pt='0.5em'>{producto.descripcion}</Text>
+            <Text fontWeight="bold" fontSize='x-large' textAlign='end' pt='0.2em' fontFamily={fontText}>
+              ${producto.precio}
+            </Text>
           </Box>
         ))}
-      </Stack>
+      </SimpleGrid>
     </Box>
   );
 };
