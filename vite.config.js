df5c193794672066cc92b15966db6090d9d6e71b
Tónga <gastongracis@gmail.com>
2023-08-19 15:49:39 -0300

    a ver 3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "http://18.231.148.75",
+    port: 3000,
+  },
+});
