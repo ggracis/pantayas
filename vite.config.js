3945d45c0d0775f8365665e434845b04afdfe794
Tónga <gastongracis@gmail.com>
2023-08-19 18:39:03 -0300

    haciendo lio

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
-import dns from "dns";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-dns.setDefaultResultOrder("verbatim");
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "http://18.231.148.75",
-    port: 3000,
-  },
 });
