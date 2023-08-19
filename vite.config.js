824d030a295404f91b2973bf33998b469fa7e972
Tónga <gastongracis@gmail.com>
2023-08-19 16:07:45 -0300

    a ver 4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import dns from "dns";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+dns.setDefaultResultOrder("verbatim");
 export default defineConfig({
   plugins: [react()],
   server: {
